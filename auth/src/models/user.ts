2e5ec66af2072fb5267e2f3c9fb498f30b7a6428
penioi <penio.ivanov@googlemail.com>
2020-05-07 15:58:10 +0100

    JWT + Cookies session + secret storage in K8s and env variables

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
-
+import { Password } from '../services/password';
 // Interface that describes what properties a User has
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
 interface UserModel extends mongoose.Model<any> {
   build(attrs: UserAttrs): UserDoc;
 }
@@ -14,14 +19,18 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
-
-interface UserAttrs {
-  email: string;
-  password: string;
-}
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { RequestValidationError } from '../errors/RequestValidationError';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/BadRequestError';
+
 const router = express.Router();
 
 router.post(
@@ -25,9 +27,20 @@ router.post(
       console.log('Email in use');
       throw new BadRequestError('Email in use!');
     }
-
     const user = User.build({ email, password });
     await user.save();
+    // Generate jwt and save to session obj
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
     res.status(201).send(user);
   }
 );
